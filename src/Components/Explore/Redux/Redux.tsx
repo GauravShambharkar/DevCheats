fd2e514251b38ab2029d2e3db7efc143e0ae2c00
Gaurav Shambharkar <shambharkargaurav04@gmail.com>
2025-11-26 11:10:35 +0530

    uibugfix

--- src/Components/Explore/Redux/Redux.tsx
+++ src/Components/Explore/Redux/Redux.tsx
@@ -1,16 +1,32 @@
 import BreadCrumb from "@/Components/BreadCrumbs/BreadCrumb";
 import React from "react";
+import TreeStructure from "../ExploreTopics/Tree/comp-567";
 
 type breadCrumbType = {
   breadCrumb: string;
 };
 
+const TopicType = {
+  topic: "Redux",
+  folders: {
+    frontend: [
+      {
+        reducers: "Reducers.tsx",
+        store: "Store.tsx",
+      },
+    ],
+  },
+};
+
 const Redux: React.FC<breadCrumbType> = ({ breadCrumb }) => {
   return (
     <>
-      <div className="flex py-2 flex-col">
+      <div className="flex py-2 flex-col gap-2 w-full h-full">
         <BreadCrumb breadCrumb={breadCrumb} />
         {/* <div>Redux</div> */}
+        <div className=" w-fit">
+          <TreeStructure TopicType={TopicType} />
+        </div>
       </div>
     </>
   );
